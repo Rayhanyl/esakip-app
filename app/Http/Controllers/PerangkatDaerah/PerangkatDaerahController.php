789d008c761a6ed1fc5889bd1287c871b93dc65d
rayhan <rayhanyuda13@gmail.com>
2024-05-28 14:19:45 +0700

    fix layouts perda

--- app/Http/Controllers/PerangkatDaerah/PerangkatDaerahController.php
+++ app/Http/Controllers/PerangkatDaerah/PerangkatDaerahController.php
@@ -10,4 +10,39 @@ class PerangkatDaerahController extends Controller
     public function index(){
         return view('perda.index');
     }
+
+    public function sasaranStrategis()
+    {
+        return view('perda.perencanaan_kinerja.sasaran_strategis');
+    }
+
+    public function sasaranProgram()
+    {
+        return view('perda.perencanaan_kinerja.sasaran_program');
+    }
+
+    public function sasaranKegiatan()
+    {
+        return view('perda.perencanaan_kinerja.sasaran_kegiatan');
+    }
+    
+    public function sasaranSubKegiatan()
+    {
+        return view('perda.perencanaan_kinerja.sasaran_subkegiatan');
+    }
+
+    public function pengukuranKinerja()
+    {
+        return view('perda.pengukuran_kinerja');
+    }
+
+    public function pelaporanKinerja()
+    {
+        return view('perda.pelaporan_kinerja');
+    }
+
+    public function evaluasiInternal()
+    {
+        return view('perda.evaluasi_internal');
+    }
 }
---
+++ resources/views/perda/pelaporan_kinerja.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('content')
+    <div id="main-content">
+        <div class="page-heading">
+            <div class="page-title">
+                <div class="row">
+                    <div class="col-12 col-md-6 order-md-1 order-last">
+                        <h3>Pelaporan Kinerja</h3>
+                        {{-- <p class="text-subtitle text-muted">
+                            Navbar will appear on the top of the page.
+                        </p> --}}
+                    </div>
+                    <div class="col-12 col-md-6 order-md-2 order-first">
+                        <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                            <ol class="breadcrumb">
+                                {{-- <li class="breadcrumb-item">
+                                    <a href="index.html">Pengukuran Kinerja</a>
+                                </li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    Layout Vertical Navbar
+                                </li> --}}
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <section class="section">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Form Pelaporan Kinerja</h4>
+                    </div>
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+    {{-- Modal --}}
+    {{-- Modal --}}
+    @push('scripts')
+        <script>
+            // 
+        </script>
+    @endpush
+@endsection
