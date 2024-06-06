85a56882115b1deab900660bba33d95d9701224b
rayhan <rayhanyuda13@gmail.com>
2024-06-06 15:55:45 +0700

    push code lhe

--- resources/views/akses_publik/evaluasi_internal/lhe.blade.php
+++ resources/views/akses_publik/evaluasi_internal/lhe.blade.php
@@ -1,1 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LHE Document</title>
+</head>
+
+<style>
+    /* Width */
+    .w-100 {
+        width: 100%;
+    }
+
+    .w-80 {
+        width: 80%;
+    }
+
+    .w-75 {
+        width: 75%;
+    }
+
+    .w-50 {
+        width: 50%;
+    }
+
+    .w-25 {
+        width: 25%;
+    }
+
+    .w-20 {
+        width: 20%;
+    }
+
+    /* Width */
+    /* Height */
+    .h-100 {
+        height: 100%;
+    }
+
+    /* Height */
+    /* Text */
+    .text-center {
+        text-align: center !important;
+    }
+
+    .text-end {
+        text-align: right !important;
+    }
+
+    .text-start {
+        text-align: left !important;
+    }
+
+    /* Text */
+    .center {
+        margin: auto;
+        width: 50%;
+    }
+
+    .text-center {
+        text-align: center;
+    }
+</style>
+
+<body>
+    <table style="width: 100%">
+        <tr>
+            <th class="w-20">
+                <img class="w-100" src="https://e-sakipraharja.majalengkakab.go.id/assets/images/logo/pemkab1.png"
+                    alt="img pemkab">
+            </th>
+            <th class="w-80">
+                <h5>PEMERINTAH KABUPATEN MAJALENGKA</h5>
+                <h3>INSPEKTORAT</h3>
+                <h6>Jln K.H Abdul Halim No. 520 Majalengka, Jawa Barat 45413,</h6>
+                <h6>Telp (0233) 281157 Laman inspektorat.majalengkakab.go.id Pos-el
+                    inspektorat@majalengkakab.go.id</h6>
+            </th>
+        </tr>
+        <tr>
+            <th colspan="2">
+                <hr style="border:3px solid black;">
+            </th>
+        </tr>
+    </table>
+
+    <table>
+        <tr>
+            Majalengka, 1 Juni 2024
+        </tr>
+    </table>
+
+
+</body>
+
+</html>
