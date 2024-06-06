92a8cf3936968c09acc9e39959d414a1d59fcb95
Zaenudin Prasetya <zaenudinprasetya@gmail.com>
2024-06-06 07:54:04 +0700

    fix migration

--- database/migrations/2024_06_01_041932_create_perda_pengukuran_kinerjas_table.php
+++ database/migrations/2024_06_01_041932_create_perda_pengukuran_kinerjas_table.php
@@ -20,11 +20,10 @@ return new class extends Migration
             $table->foreignId('sasaran_strategis_indikator_id')->onDelete('cascade')->nullable();
             $table->foreignId('sasaran_sub_kegiatan_id')->onDelete('cascade')->nullable();
             $table->foreignId('sasaran_sub_kegiatan_indikator_id')->onDelete('cascade')->nullable();
-            $table->foreignId('sasaran_sub_kegiatan_target')->nullable();
+            $table->double('sasaran_sub_kegiatan_target')->nullable();
             $table->double('realisasi')->nullable();
             $table->enum('karakteristik', ['1', '2'])->nullable();
             $table->double('capaian')->nullable();
-            $table->foreignId('anggaran_sub_kegiatan_id')->onDelete('cascade')->nullable();
             $table->double('anggaran_pagu')->nullable();
             $table->double('anggaran_realisasi')->nullable();
             $table->double('anggaran_capaian')->nullable();
