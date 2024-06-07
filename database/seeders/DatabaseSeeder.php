5320d5dec50777126ac50eeacfa39346902f3837
rayhan <rayhanyuda13@gmail.com>
2024-06-08 04:30:34 +0700

    hide user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
+            // UserSeeder::class,
             PenanggungJawabSeeder::class,
-            SatuanSeeder::class,
+            // SatuanSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
