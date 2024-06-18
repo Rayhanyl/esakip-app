8ce5f62f605f1fecf9f089259f3e0b2856caf01a
rayhan <rayhanyuda13@gmail.com>
2024-06-18 17:43:25 +0700

    push code

--- app/Http/Controllers/Aspu/AspuBerandaController.php
+++ app/Http/Controllers/Aspu/AspuBerandaController.php
@@ -10,7 +10,7 @@ class AspuBerandaController extends Controller
     public function __invoke()
     {
         $labels = ['2019', '2020', '2021', '2022', '2023'];
-        $data = [67, 67.18, 67.08, 67.1, 68];
+        $data = [67, 67.18, 67.08, 67.1, 70.1];
         return view('aspu.beranda.index', compact('labels', 'data'));
     }
 }
--- resources/views/aspu/perencanaan/pemkab/rkpd/index.blade.php
+++ resources/views/aspu/perencanaan/pemkab/rkpd/index.blade.php
@@ -37,7 +37,7 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Tabel Renja</h4>
+                            <h4 class="card-title">Tabel RKPD</h4>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
