dadab4563eb012553dad08727b7896b005a45507
Zaenudin Prasetya <zaenudinprasetya@gmail.com>
2024-07-03 10:19:54 +0700

    fix select

--- resources/views/admin/inspek/self_assesment/index.blade.php
+++ resources/views/admin/inspek/self_assesment/index.blade.php
@@ -133,7 +133,8 @@
                                                                     </option>
                                                                     @foreach ($kriteria->answers as $answer)
                                                                         <option value="{{ $answer->bobot }}"
-                                                                            {{ (float) $answer->bobot == (float) ($kriteria->inspek_status ?? $kriteria->status) ? 'selected' : '' }}>
+                                                                            {{ (float) $answer->bobot == (float) ($kriteria->inspek_status ?? $kriteria->status) ? 'selected' : '' }}
+                                                                            title="{{ $answer->jawaban }} ({{ $answer->bobot }})">
                                                                             {{ $answer->jawaban }}
                                                                             ({{ $answer->bobot }})
                                                                         </option>
